2f2e78736cac6c69cdab8829f99b0bbf312c3622
Joshua Rossi <maximojoshuarossi@gmail.com>
2013-07-05 17:44:08 -0400

    initial commit of buttonwood_advanced

---
+++ client/compatibility/twitter.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: joshua.rossi
+ * Date: 7/5/13
+ * Time: 12:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
